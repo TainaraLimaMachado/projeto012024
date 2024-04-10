b19332efb76e7d5196983db6789158c13423b739
TainaraLimaMachado <111454243+TainaraLimaMachado@users.noreply.github.com>
2024-04-10 09:23:06 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
 const botoes = document.querySelectorAll(".botao");
-console.log(botoes)
-for(let i=0; i <  botoes.length; i++){
-    console.log(i);
-}botoes[i].onclick = function(){
-    
-    botoes[i].classList.add("ativo");
-}
-for(let j=0;j<botoes.length;j++){
-            botoes[j].classList.remove("ativo");
- }
-const botoes = document.querySelectorAll(".botao");
 
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
@@ -21,5 +10,3 @@ for(let i=0;i <botoes.length;i++){
         botoes[i].classList.add("ativo");
     }
 }
-
-
